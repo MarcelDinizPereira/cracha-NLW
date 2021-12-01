959e164837e04844df119987d52ce4902d44b2cb
JustDoIt <59229675+pixelrgb255@users.noreply.github.com>
2021-12-01 07:02:24 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+const LinksSocialMedia = {
+  github: 'pixelrgb255',
+  twitter: 'twit_ming',
+  linkedin: 'in/marcel-diniz-82bb13149'
+}
+
+function changeSocialMedialLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https://${social}.com/${LinksSocialMedia[social]}`
+  }
+
+  //document.getElementById('userName').textContent = 'Marcelo'
+  // userName.textContent = 'Marcelo' //simplified version
+}
+
+changeSocialMedialLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${LinksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      document.getElementById('userName').textContent = data.name
+      document.getElementById('userBio').textContent = data.bio
+      document.getElementById('userLink').href = data.html_url
+      document.getElementById('userImage').src = data.avatar_url
+      document.getElementById('userLogin').textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
